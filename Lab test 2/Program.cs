700131d6bb3a2fd0beb16afd92ef206fa1c76c61
visualstudio <visualstudio@122-PC35>
2019-03-06 10:19:03 -0500

    initial commit 730263 726796

--- Lab test 2/Program.cs
+++ Lab test 2/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_test_2
+namespace DelegateAndExercises
+
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+}
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //TO DO: Call Method2 from the DelegateExercises Class
         }
     }
 }
